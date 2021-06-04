f5697f3ba7ace549a19cb3641982a67a718b575a
zvachniy <d.kachalov@outlook.com>
2021-06-04 17:24:18 +0300

    try trough .lower()

--- task14.py
+++ task14.py
@@ -4,15 +4,16 @@ import re
 
 in_text = "Lorem"
 
-txt = re.search(r"\W", in_text)
+# txt = re.sub(pattern='[A-Z]', repl='[a-z]', string=in_text, count=len(in_text))
 
 
 def find_latin_set(text):
+    # text = re.sub(pattern='[A-Z]', repl='[a-z]', string=text, count=len(text))
     latin_set = set()
     for letter in text:
-
-        latin_set.update(letter)
+        l=letter.lower
+        latin_set.update(l)
     return print(latin_set)
 
 
-find_latin_set(txt)
+find_latin_set(in_text.lower)
